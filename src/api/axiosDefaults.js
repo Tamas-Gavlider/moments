821eaabfcbb6a1bda58f2bf4d799e6a6cafd7df5
Tamas Gavlider <6mv8g9c8@students.codeinstitute.net>
2024-12-18 19:11:25 +0000

    m

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -2,4 +2,7 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://moments-pp5-4e7d3f875a72.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
-axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = false;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
