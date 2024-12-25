983b0bda17bb68a9ee8828df09357e8f206c683b
Tamas Gavlider <6mv8g9c8@students.codeinstitute.net>
2024-12-25 20:37:38 +0000

    set tests

---
+++ src/mocks/handlers.js
@@ -0,0 +1,23 @@
+import { rest } from "msw";
+
+const baseURL = "https://drf-api-react-4996193a1e99.herokuapp.com/";
+
+export const handlers = [
+  rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        pk: 2,
+        username: "brian",
+        email: "",
+        first_name: "",
+        last_name: "",
+        profile_id: 2,
+        profile_image:
+          "https://res.cloudinary.com/dfi7tw6ox/image/upload/v1/media/../default_profile_jx7eer",
+      })
+    );
+  }),
+  rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+];
